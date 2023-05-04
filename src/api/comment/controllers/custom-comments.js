73810283d7d59ed8a60abb96e241dde2b36c88bd
Iryna Pec <118050604+irapets777@users.noreply.github.com>
2023-05-04 12:02:45 +0300

    test

---
+++ src/api/comment/controllers/custom-comments.js
@@ -0,0 +1,10 @@
+// const { createCoreController } = require("@strapi/strapi").factories;
+
+// const moduleCommentId = "api::comment.comment";
+
+// module.exports = createCoreController(moduleCommentId, ({ strapi }) => ({
+//     test: async (ctx) => {
+//         console.log("TEST");
+//     },
+
+// }));
